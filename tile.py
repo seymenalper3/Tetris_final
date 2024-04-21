225c66634cdcdc5e94197b653b12cabcf754b363
sdriza <aslanriza.81@gmail.com>
2024-04-21 21:35:05 +0300

    Enes_last_commit

--- tile.py
+++ tile.py
@@ -1,36 +1,78 @@
-import stddraw as stddraw  # used for drawing the tiles to display them
-from color import Color  # used for coloring the tiles
+import stddraw # the stddraw module is used as a basic graphics library
+from color import Color # used for coloring the tile and the number on it
+from point import Point # used for representing the position of the tile
+import copy as cp # the copy module is used for copying tile positions
+import math # math module that provides mathematical functions
+import random
+import numpy as np
 
-# A class for modeling numbered tiles as in 2048
+# Class used for representing numbered tiles as in 2048
 class Tile:
-   # Class variables shared among all Tile objects
+   # Class attributes shared among all Tile objects
    # ---------------------------------------------------------------------------
-   # the value of the boundary thickness (for the boxes around the tiles)
+   # value used for the thickness of the boxes (boundaries) around the tiles
    boundary_thickness = 0.004
-   # font family and font size used for displaying the tile number
-   font_family, font_size = "Arial", 14
+   # font family and size used for displaying the tile number
+   font_family, font_size = "Verdana", 14
 
-   # A constructor that creates a tile with 2 as the number on it
-   def __init__(self):
-      # set the number on this tile
-      self.number = 2
-      # set the colors of this tile
-      self.background_color = Color(151, 178, 199)  # background (tile) color
-      self.foreground_color = Color(0, 100, 200)  # foreground (number) color
-      self.box_color = Color(0, 100, 200)  # box (boundary) color
+   # Constructor that creates a tile at a given position with 2 as its number
+   def __init__(self, position = Point(0, 0)): # (0, 0) is the default position
+      # Assigns the random number of the tile 2 or 4 for initial
+      numbers = [2, 4]
+      # Sets a background color for each possible number
+      self.colors = [Color(239, 230, 221),
+                     Color(239, 227, 205),
+                     Color(247, 178, 123),
+                     Color(247,150 ,99),
+                     Color(247,124 ,90),
+                     Color(247,93  ,59),
+                     Color(239,205 ,115),
+                     Color(239,206 ,99),
+                     Color(239,198 ,82),
+                     Color(238,198,66),
+                     Color(239,194,49),
+                     Color(60 ,58,51)]
+      self.num = int(np.random.choice(numbers, 1))
+      self.number = self.num
+      # set the colors of the tile
+      self.background_color = Color(239, 230, 221)# background (tile) color
+      self.foreground_color = Color(0, 0,0) # foreground (number) color
+      self.boundary_color = Color(128, 128, 128) # boundary (box) color
+      # set the position of the tile as the given position
+      self.position = Point(position.x, position.y)
 
-   # A method for drawing this tile at a given position with a given length
-   def draw(self, position, length=1):  # length defaults to 1
+   # Setter method for the position of the tile
+   def set_position(self, position):
+      # set the position of the tile as the given position
+      self.position = cp.copy(position)
+
+   # Getter method for the position of the tile
+   def get_position(self):
+      # return the position of the tile
+      return cp.copy(self.position)
+
+   # Method for moving the tile by dx along the x axis and by dy along the y axis
+   def move(self, dx, dy):
+      self.position.translate(dx, dy)
+
+   # Method for drawing the tile
+   def draw(self, position = None):
+      if position is None:
+          position = self.position
       # draw the tile as a filled square
       stddraw.setPenColor(self.background_color)
-      stddraw.filledSquare(position.x, position.y, length / 2)
-      # draw the bounding box around the tile as a square
-      stddraw.setPenColor(self.box_color)
+      stddraw.filledSquare(self.position.x, self.position.y, 0.5)
+      # draw the bounding box of the tile as a square
+      stddraw.setPenColor(self.boundary_color)
       stddraw.setPenRadius(Tile.boundary_thickness)
-      stddraw.square(position.x, position.y, length / 2)
+      stddraw.square(self.position.x, self.position.y, 0.5)
       stddraw.setPenRadius()  # reset the pen radius to its default value
       # draw the number on the tile
       stddraw.setPenColor(self.foreground_color)
       stddraw.setFontFamily(Tile.font_family)
       stddraw.setFontSize(Tile.font_size)
-      stddraw.text(position.x, position.y, str(self.number))
+      stddraw.boldText(self.position.x, self.position.y, str(self.number))
+
+   # Updates the background of the tile according to tile's number
+   def updateColor(self, num):
+      self.background_color = self.colors[int(math.log2(num)) - 1]
